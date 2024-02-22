dd7709b36d64a33b35bf05cd0bf91809717d2c93
Issam <issam.sisbane@gmail.com>
2024-02-22 21:59:33 +0100

    update addClip route  to use database and Animals json

--- src/functions/addClip.ts
+++ src/functions/addClip.ts
@@ -1,31 +1,54 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import CosmosDBClient from "../cosmosDBClient";
-import { Clip } from "../models/clip";
+import { Clip, verifyClip } from "../models/clip";
+import * as fs from 'fs';
 
 export async function addClip(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
-
-    //const name = request.query.get('name') || await request.text() || 'world';
+    context.log(`Starting the clip creation...`);
 
     const body: any = await request.json();
 
-    if (!body.path || !body.title || !body.content || !body.type || !body.ttll) {
+    if (!verifyClip(body)) {
         return {
             status: 400,
-            body: "Please pass a path, title, content, type and ttl in the request body"
+            body: "Please pass a path, title, content, type and timeToLive in the request body"
         };
     }
 
     const clip = body as Clip;
-
     const cosmosDBClient = new CosmosDBClient();
+
+    // get a random unused animal
+    context.log(`Finding an unused animal..."`);
+    const items = await cosmosDBClient.container.items.readAll().fetchAll();
+    const usableAnimals = getUsableAnimals(items.resources as Clip[]);
+    const randomIndex = Math.floor(Math.random() * usableAnimals.length);
+    const randomAnimal = usableAnimals[randomIndex];
+    clip.path = randomAnimal;
+
+    // add the clip to the database
     await cosmosDBClient.container.items.upsert(clip);
 
-    return { body: `Hellon, ${body}!` };
+    context.log(`New clip created...`);
+    return { body: `Clip added to path ${clip.path}` };
 };
+
+function readAnimalsJsonFileSync() {
+    const animals = fs.readFileSync("src/helpers/animals.json", 'utf8');
+    return JSON.parse(animals);
+}
+
+function getUsableAnimals(clips: Clip[]) {
+    const animals = readAnimalsJsonFileSync();
+
+    return animals.filter(animal => {
+        return clips.every(item => item.path !== animal);
+    });
+}
 
 app.http('addClip', {
     methods: ['POST'],
     authLevel: 'anonymous',
+    route: 'clip/add',
     handler: addClip
 });
